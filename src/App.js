bb5530f84160e20f82257c99415456949f5f3abc
Chris Garcia <chrisgarcia769@gmail.com>
2019-03-12 01:22:24 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import ImageGrid from "./components/ImageGrid";
+//import MultiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>App</h1>
+      <div>
+        <ImageGrid />
       </div>
     );
   }
---
+++ src/components/ImageGrid.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const styles = {
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9,
+    marginTop: "30"
+  }
+};
+
+class ImageGrid extends Component {
+  state = {
+    photos: []
+  };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/photos").then(res => {
+      this.setState(() => {
+        return {
+          photos: res.data
+        };
+      });
+    });
+  }
+
+  render() {
+    const cards = [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24
+    ];
+    console.log(this.state.photos[0]);
+    return (
+      <Grid container spacing={40}>
+        {cards.map(card => (
+          <Grid item key={card} sm={6} md={4} lg={3}>
+            <Card>
+              <CardMedia
+                image="https://www.gstatic.com/webp/gallery3/1.png" // eslint-disable-line max-len
+                title="Image title"
+                style={styles.media}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Heading
+                </Typography>
+                <Typography>
+                  This is a media card. You can use this section to describe the
+                  content.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small" color="primary">
+                  View
+                </Button>
+                <Button size="small" color="primary">
+                  Edit
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
+}
+
+export default ImageGrid;
