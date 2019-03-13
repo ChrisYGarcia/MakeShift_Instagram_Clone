a5772ae2430eb11aeb74cf2b975e510785ba9d08
Chris Garcia <chrisgarcia769@gmail.com>
2019-03-12 21:45:13 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import ImageGrid from "./components/ImageGrid";
-import newImageGrid from "./components/newImageGrid";
-//import MultiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+//import NewImageGrid from "./components/ImageGrid";
+import NewImageGrid from "./components/NewImageGrid";
+import MultiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <newImageGrid />
+        <NewImageGrid />
       </div>
     );
   }
